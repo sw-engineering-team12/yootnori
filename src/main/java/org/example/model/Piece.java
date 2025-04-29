83465e5812a82bba9eff2e1c964fbe420ed0b055
DOGJHDOG <junhyung031115@gmail.com>
2025-04-29 15:58:30 +0900

    feat - Piece Model Attribute 작성

--- src/main/java/org/example/model/Piece.java
+++ src/main/java/org/example/model/Piece.java
@@ -1,4 +1,34 @@
 package org.example.model;
 
+import java.util.ArrayList;
+
 public class Piece {
+    private String id;
+    private Player player;
+    private Position currentPosition;
+    private Boolean isComplete;
+    private Boolean isOwnhand;
+    private ArrayList<Piece> stackPieces;
+
+    //생성자
+    public Piece(String id, Player player) {
+        this.id = id;
+        this.player = player;
+        this.currentPosition = null;
+        this.isComplete = false;
+        this.isOwnhand = true;
+        this.stackPieces = new ArrayList<Piece>();
+    }
+
+    // 말 움직임
+
+    // 말 업기
+
+    // 말 잡기
+
+    //말 꺼내기
+
+    //말 골인
+
+
 }
